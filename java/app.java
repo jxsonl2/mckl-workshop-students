4c4cf41814fa68dba46c00e22a748c4cf7d6b13e
isaaclim <isaacpeanutbutter@gmail.com>
2022-07-14 16:45:36 +0800

    isaac test

--- java/app.java
+++ java/app.java
@@ -5,5 +5,6 @@
 class app {
     public static void main(String[] args) {
         System.out.println("Hello World!"); //Display the string.
+	  System.out.println("Isaac"); //Display the string.
     }
 }
